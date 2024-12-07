4bb0a4701dde9fbc74dd92e2b3b43e23e81d3df5
Tim Gallant <tim@lilt.com>
2024-12-06 23:13:33 -0800

    adds parsec.el

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd "lisp"
-emacs -batch -l aoc2024.el -l $1 -f ert-run-tests-batch-and-exit
+emacs -batch -l aoc2024.el -l parsec.el -l $1 -f ert-run-tests-batch-and-exit
 STATUS=$?
 cd -
 exit $STATUS
